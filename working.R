24274beb338c12ee39cf2116f9e376bee724b941
Jason Foster <c-jason.foster@skykick.com>
2017-06-19 12:50:02 -0600

    Removing unnecessary files

--- working.R
+++
@@ -1,7 +0,0 @@
-setwd("c:/devR/Capstone/")
-source("./Prediction.R")
-dbDir <- file.path(getwd(), "sqlite", "ngrams.sqlite")
-dbConn <- dbConnect(SQLite(), dbDir)
-setwd(file.path(getwd(), "dsci-benchmark-master"))
-source("./Benchmark.R")
-
